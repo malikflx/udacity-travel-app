216ddfc978d208a1e76fdb4362e91c47772b993c
Malik Felix <malikfelix50@gmail.com>
2021-07-01 18:52:02 -0400

    enable service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -51,6 +51,6 @@ module.exports = {
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
-    // new WorkboxPlugin.GenerateSW()
+    new WorkboxPlugin.GenerateSW()
   ],
 }
